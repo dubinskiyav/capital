e1211151d0a77c5b26eca1d42324dac5c0b6ed8d
dav <dav@gelicon.biz>
2021-01-15 00:06:18 +0500

    DatabaseCreate

--- src/main/java/biz/gelicon/capital/repository/MeasureRepository.java
+++ src/main/java/biz/gelicon/capital/repository/MeasureRepository.java
@@ -22,4 +22,38 @@ public class MeasureRepository implements TableRepository<Measure>{
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+    public int load() {
+        insert(new Measure(1, "Вес"));
+        insert(new Measure(2, "Расстояние"));
+        insert(new Measure(3, "Время"));
+        insert(new Measure(4, "Сила тока"));
+        insert(new Measure(5, "Температура"));
+        insert(new Measure(6, "Количество вечества"));
+        insert(new Measure(7, "Сила света"));
+        insert(new Measure(11, "Плоский угол"));
+        insert(new Measure(12, "Телесный угол"));
+        insert(new Measure(13, "Температура по шкале Цельсия"));
+        insert(new Measure(14, "Частота"));
+        insert(new Measure(15, "Сила"));
+        insert(new Measure(16, "Энергия"));
+        insert(new Measure(17, "Мощность"));
+        insert(new Measure(18, "Давление"));
+        insert(new Measure(19, "Световой поток"));
+        insert(new Measure(20, "Освещенность"));
+        insert(new Measure(21, "Электрический заряд"));
+        insert(new Measure(22, "Разность потенциалов"));
+        insert(new Measure(23, "Сопротивление"));
+        insert(new Measure(24, "Электроёмкость"));
+        insert(new Measure(25, "Магнитный поток"));
+        insert(new Measure(26, "Магнитная индукция"));
+        insert(new Measure(27, "Индуктивность"));
+        insert(new Measure(28, "Электрическая проводимость"));
+        insert(new Measure(29, "Активность (радиоактивного источника)"));
+        insert(new Measure(30, "Поглощённая доза ионизирующего излучения"));
+        insert(new Measure(31, "Эффективная доза ионизирующего излучения"));
+        insert(new Measure(32, "Активность катализатора"));
+        logger.info("31 measures loaded");
+        return 31;
+    }
+
 }
--- src/main/java/biz/gelicon/capital/utils/Test01.java
+++ src/main/java/biz/gelicon/capital/utils/Test01.java
@@ -43,84 +43,16 @@ public class Test01 {
     public void test1() {
         System.out.println("Tests started");
 
-        Measureunit measureunit = measureunitRepository.findById(2);
-        Integer measureCount = measureRepository.count();
-        List<Measureunit> measureunitList = measureunitRepository.findAll();
-        // Каждая операция - в своей транзакции, так как TableRepository аннотирован как
-        // @Transactional(propagation = Propagation.REQUIRED)
-        measureunitRepository.deleteAll();
-        measureRepository.deleteAll();
-        unitmeasurerecalcRepository.deleteAll();
-        unitmeasureRepository.deleteAll();
         // Тесты
         defaultTransactionDefinition = new DefaultTransactionDefinition();
         transactionStatus = transactionManager.getTransaction(defaultTransactionDefinition);
         try {
-            measureRepository.delete(1);
-            Measure measure = new Measure(1, "Длина");
-            measureRepository.insert(measure);
-            measureRepository.delete(measure);
-            measure = new Measure(1, "Ширина");
-            measureRepository.insert(measure);
             transactionManager.commit(transactionStatus);
         } catch (Exception e) {
             String errText = String.format("Error. Transaction will be rolled back");
             logger.error(errText, e);
             transactionManager.rollback(transactionStatus);
         }
-
-        transactionStatus = transactionManager.getTransaction(defaultTransactionDefinition);
-        try {
-            measureRepository.delete(1);
-            Measure measure = new Measure(1, "Высота");
-            measureRepository.insert(measure);
-            measure.setName("Глубина");
-            measureRepository.update(measure);
-            measure = new Measure(2, "Высота");
-            measureRepository.insert(measure);
-            transactionManager.commit(transactionStatus);
-        } catch (Exception e) {
-            String errText = String.format("Error. Transaction will be rolled back");
-            logger.error(errText, e);
-            transactionManager.rollback(transactionStatus);
-            transactionManager.rollback(transactionStatus);
-        }
-
-        //UnitmeasureRepository unitmeasureRepository =
-        //        CapitalApplication.getApplicationContext().getBean(UnitmeasureRepository.class);
-        Unitmeasure unitmeasure = new Unitmeasure();
-        unitmeasure.setId(1);
-        unitmeasure.setName("Метр");
-        unitmeasure.setShortName("м.");
-        unitmeasureRepository.insert(unitmeasure);
-        unitmeasure.setId(null);
-        unitmeasure.setName("Хуиля");
-        unitmeasure.setShortName("миля");
-        unitmeasureRepository.insertOrUpdate(unitmeasure);
-        unitmeasure.setName("Миля");
-        unitmeasure.setShortName("миля");
-        unitmeasureRepository.insertOrUpdate(unitmeasure);
-        unitmeasure.setShortName("мил.");
-        unitmeasureRepository.set(unitmeasure);
-        unitmeasure.setId(2);
-        unitmeasure.setName("Литр");
-        unitmeasure.setShortName("л");
-        unitmeasureRepository.set(unitmeasure);
-        unitmeasure.setId(null);
-        unitmeasure.setName("Тонна");
-        unitmeasure.setShortName("тн");
-        unitmeasureRepository.insertOrUpdate(unitmeasure);
-
-        measureunit = new Measureunit();
-        measureunit.setId(1);
-        measureunit.setMeasureId(1);
-        measureunit.setUnitmeasureId(1);
-        measureunit.setPriority(0);
-        measureunitRepository.insert(measureunit);
-        measureunit = new Measureunit(2, 2, 1, 0);
-        measureunitRepository.insert(measureunit);
-
-        measureRepository.findAll().forEach(System.out::println);
 
         System.out.println("Tests ended");
 
