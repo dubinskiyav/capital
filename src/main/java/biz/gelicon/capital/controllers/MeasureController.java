0b616d053616794cc880ffc04ac7f57d1d96d66c
dav <dav@gelicon.biz>
2021-02-16 09:16:07 +0500

    unitmeasure

--- src/main/java/biz/gelicon/capital/controllers/MeasureController.java
+++ src/main/java/biz/gelicon/capital/controllers/MeasureController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Контроллер Справочника единиц измерения
